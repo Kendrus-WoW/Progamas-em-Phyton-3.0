62b52351b29330180e08766ce3f8cc13b65e831e
Kendrus-WoW <180138469+Kendrus-WoW@users.noreply.github.com>
2024-09-16 04:40:23 -0300

    Update Calculadora_ST.py

--- Calculadora_ST.py
+++ Calculadora_ST.py
@@ -4,16 +4,16 @@ import streamlit as st
 st.title('Calculadora de Descontos')
 
 # Solicita o valor do produto ao usuário
-n1 = st.number_input('Coloque o valor do produto aqui:')
+n1 = st.number_input('Coloque o valor do produto aqui: ')
 
 # Solicita a porcentagem de desconto ao usuário
-n2 = st.number_input('Coloque a porcentagem de desconto aqui:')
+n2 = st.number_input('Coloque a porcentagem de desconto aqui: ')
 
 # Calcula o preço final após aplicar o desconto
 soma = n1 - (n1 * n2) / 100
 
 # Mostra o preço do produto, o desconto, e por último qual o preço do produto com o desconto
-st.write(f'O produto que era R${n1:.2f} com o desconto de {n2}% ficou R${soma:.2f}')
+st.write(f'O produto que era R$ {n1:.2f} com o desconto de {n2} % ficou R$ {soma:.2f}')
 
 # Mostra se o produto está com um bom desconto, abaixo de 500, 300 e 100
 if soma > 500:
